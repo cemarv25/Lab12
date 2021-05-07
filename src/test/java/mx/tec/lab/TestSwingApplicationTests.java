277d7ad81b9b108930633d08b51f3fab2b7b9e9b
cemarv25 <cesar.martinez0010@outlook.com>
2021-05-07 11:35:13 -0600

    Initial commit, setup

---
+++ src/test/java/mx/tec/lab/TestSwingApplicationTests.java
@@ -0,0 +1,27 @@
+package mx.tec.lab;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSwingApplicationTests {
+	TestSwingApplication application = new TestSwingApplication();
+
+	@Test
+	void givenTwoIntegers_WhenSum_ThenCorrectResult() {
+		// Given 5 and 7
+		application.numberOneTextField.setText("5");
+		application.numberTwoTextField.setText("7");
+		String expectedResult = "12";
+		
+		// When operation is sum
+		application.operationButton.doClick();
+		
+		// Then result is 12
+		String actualResult = application.resultTextField.getText();
+		assertEquals(expectedResult, actualResult);
+	}
+
+}
